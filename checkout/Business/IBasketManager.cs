17fc2ee2de5654f1eb073a99286922ebb9ddbde5
huguestre <hugo.etre@gmail.com>
2019-08-22 11:32:01 +0100

    add basket manager + getotal function (no offers)

---
+++ checkout/Business/IBasketManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using checkout.Models;
+
+namespace checkout.Business
+{
+    public interface IBasketManager
+    {
+        decimal GetTotal(Basket basket);
+    }
+}
