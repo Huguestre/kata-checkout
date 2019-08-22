ec37f8809073758a2cbd9621826d364f90717a46
huguestre <hugo.etre@gmail.com>
2019-08-22 18:20:20 +0100

    added support to get basket total including offer more tests

--- checkout/Business/IBasketManager.cs
+++ checkout/Business/IBasketManager.cs
@@ -1,13 +1,15 @@
-﻿using System;
+﻿using checkout.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using checkout.Models;
 
 namespace checkout.Business
 {
     public interface IBasketManager
     {
         decimal GetTotal(Basket basket);
+
+        decimal GetTotal(Basket basket, IOfferManager offerManager);
     }
 }
--- checkout/Business/IOfferManager.cs
+++ checkout/Business/IOfferManager.cs
@@ -1,13 +1,15 @@
-﻿using System;
+﻿using checkout.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using checkout.Models;
 
 namespace checkout.Business
 {
     public interface IOfferManager
     {
         Offer GetOfferForItem(Item item);
+
+        List<Offer> GetOffers();
     }
 }
